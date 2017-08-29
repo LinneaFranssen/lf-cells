eb090486f6f0127f42a2c7e3b497716e06fdb839
Jack Nichol <work@MacBook.local>
2017-08-29 16:21:57 +0100

    Initial Commit

---
+++ lf-cells/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lf-cells
+//
+//  Created by Work on 29/08/2017.
+//  Copyright © 2017 Jack Nichol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
